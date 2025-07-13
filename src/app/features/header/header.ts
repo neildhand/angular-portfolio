38bf81a24cb12a963390d5a43312c0f5e2b7826d
ksz3 <153975900+ksz3@users.noreply.github.com>
2025-07-13 16:11:48 -0500

    future proof by removing ngClass

--- src/app/features/header/header.ts
+++ src/app/features/header/header.ts
@@ -1,9 +1,8 @@
-import { Component, computed, signal } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { Component, computed, output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [NgClass],
+  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
@@ -11,22 +10,23 @@ export class Header {
   menuOpen = false;
   isDarkMode = signal(true);
   darkModeClasses = {
-    header: 'from-stone-800 via-zinc-900 to-stone-800',
-    navNameSpan: 'text-white',
-    navLi: 'text-white hover:text-indigo-300',
-    resumeBtn: 'bg-stone-600 hover:bg-zinc-500 text-white',
-    darkModeToggleBtn: 'text-white',
-    svgColor: 'white'
+    header: 'from-stone-800 via-zinc-900 to-stone-800 ',
+    navNameSpan: 'text-white ',
+    navLi: 'text-white hover:text-indigo-300 ',
+    resumeBtn: 'bg-stone-600 hover:bg-zinc-500 text-white ',
+    darkModeToggleBtn: 'text-white ',
+    svgColor: 'white '
   };
   lightModeClasses = {
-    header: 'from-zinc-300 via-stone-400 to-zinc-300',
-    navNameSpan: 'text-black',
-    navLi: 'text-black hover:text-indigo-800',
-    resumeBtn: 'bg-zinc-200 hover:bg-stone-400 text-black',
-    darkModeToggleBtn: 'text-black',
-    svgColor: 'black'
+    header: 'from-zinc-300 via-stone-400 to-zinc-300 ',
+    navNameSpan: 'text-black ',
+    navLi: 'text-black hover:text-indigo-800 ',
+    resumeBtn: 'bg-zinc-200 hover:bg-stone-400 text-black ',
+    darkModeToggleBtn: 'text-black ',
+    svgColor: 'black '
   }
   currentModeClass = computed(() => this.isDarkMode() ? this.darkModeClasses : this.lightModeClasses);
+  isDarkModeOutput = output<boolean>();
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
@@ -34,6 +34,6 @@ export class Header {
 
   toggleDarkMode() {
     this.isDarkMode.set(!this.isDarkMode());
-    // TODO: create light and dark mode
+    this.isDarkModeOutput.emit(this.isDarkMode());
   }
 }
