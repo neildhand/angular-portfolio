d7bfede9ac00e6085f6c363de6c95cc14ba4331a
ksz3 <153975900+ksz3@users.noreply.github.com>
2025-07-13 00:27:24 -0500

    set up header nav with light/dark mode

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Header } from "./features/components/header/header";
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
